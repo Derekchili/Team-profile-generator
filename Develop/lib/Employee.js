948df9b971031ebec79ba7360a4ff77bf0727e09
derek <derekchili@gmail.com>
2023-04-22 10:32:15 -0700

    updated index JS and modified other files

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -6,6 +6,19 @@ class Employee {
         this.id = id;
         this.email = email;
         
-    }
+    } 
 }
 module.exports = Employee;
+
+  // getName() {
+    //     return this.name;
+    // }
+    // getId() {
+    //     return this.id;
+    // }
+    // getEmail() {
+    //     return this.email;
+    // }
+    // getRole() {
+    //     return 'Employee'
+    // }
